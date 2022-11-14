b1f8bea1648639c98f7a47a0be4961b21fd2b114
Mark Fisher <mark.j.fisher@gmail.com>
2022-11-14 18:45:29 +0000

    Clean up whitespace in assembly.

--- include/mainargs-atari.h
+++ include/mainargs-atari.h
@@ -1,5 +1,4 @@
 #pragma code_seg(CLICode)
 #pragma data_seg(CLIData)
 
-
 void parseArgs();
--- lib/mainargs-atari.c
+++ lib/mainargs-atari.c
@@ -5,12 +5,12 @@
 #pragma data_seg(CLIData)
 
 void parseArgs() {
-	get_args(detect_dos());
+  get_args(detect_dos());
 }
 
 char detect_dos() {
-	char dos_type = 0xff;
-	kickasm(uses dos_type) {{
+  char dos_type = 0xff;
+  kickasm(uses dos_type) {{
 
 .const SPARTADOS    = 0
 .const REALDOS      = 1
@@ -27,68 +27,73 @@ char detect_dos() {
 
 .const DOSVEC  = $0A
 .const DOS     = $0700
-		
-		lda     DOS
-		cmp     #'S'            // SpartaDOS
-		beq     spdos
-		cmp     #'M'            // MyDOS
-		beq     mydos
-		cmp     #'X'            // XDOS
-		beq     xdos
-		cmp     #'R'            // RealDOS
-		beq     rdos
+    
+    lda     DOS
+    cmp     #'S'            // SpartaDOS
+    beq     spdos
+    cmp     #'M'            // MyDOS
+    beq     mydos
+    cmp     #'X'            // XDOS
+    beq     xdos
+    cmp     #'R'            // RealDOS
+    beq     rdos
 
-		lda     #$4C            // probably default (MJF: 6502 JMP = $4c)
-		ldy     #COMTAB
-		cmp     (DOSVEC),y
-		bne     done
-		ldy     #ZCRNAME
-		cmp     (DOSVEC),y
-		bne     done
+    lda     #$4C            // probably default (MJF: 6502 JMP = $4c)
+    ldy     #COMTAB
+    cmp     (DOSVEC),y
+    bne     done
+    ldy     #ZCRNAME
+    cmp     (DOSVEC),y
+    bne     done
 
-		ldy     #6              // OS/A+ has a jmp here
-		cmp     (DOSVEC),y
-		beq     done			// MJF: is this correct? seem to be comparing to 
-		lda     #OSADOS
-		bne     _set
+    ldy     #6              // OS/A+ has a jmp here
+    cmp     (DOSVEC),y
+    // MJF: is this correct? seem to be comparing to #$4C and if it's equal finishes rather than setting OSADOS
+    beq     done
+    lda     #OSADOS
+    bne     __set
 
-spdos:  lda     DOS+3           // 'B' in BW-DOS
-		cmp     #'B'
-		bne     spdos_real
-		lda     DOS+4           // 'W' in BW-DOS
-		cmp     #'W'
-		bne     spdos_real
+spdos:
+    lda     DOS+3           // 'B' in BW-DOS
+    cmp     #'B'
+    bne     spdos_real
+    lda     DOS+4           // 'W' in BW-DOS
+    cmp     #'W'
+    bne     spdos_real
 
-		lda     #BWDOS
-		// the following 2C bytes avoid the need for a JMP, as they do a harmless BIT instruction that ALSO eats the 2 bytes after them
-		// Thus instead of any jump instructions, a bunch of effective NOP commands happen until the final _set location is hit.
-		.byte   $2C             // BIT <abs>
+    // the following 2C bytes avoid the need for a JMP, as they do a harmless BIT instruction that ALSO eats
+    // the 2 bytes after them. Thus instead of any jump instructions, a bunch of effective NOP commands happen
+    // until the final _set location is hit.
+    lda     #BWDOS
+    .byte   $2C             // BIT <abs>
 
 spdos_real:
-		lda     #SPARTADOS
-		.byte   $2C             // BIT <abs>
+    lda     #SPARTADOS
+    .byte   $2C             // BIT <abs>
 
-mydos:  lda     #MYDOS
-		.byte   $2C             // BIT <abs>
+mydos:
+    lda     #MYDOS
+    .byte   $2C             // BIT <abs>
 
-rdos:   lda     #REALDOS
-		.byte   $2C             // BIT <abs>
+rdos:
+    lda     #REALDOS
+    .byte   $2C             // BIT <abs>
 
-xdos:   lda     #XDOS
-_set:   sta     dos_type
+xdos:
+    lda     #XDOS
+__set:
+    sta     dos_type
 done:   
-		// There will be an rts done for us
-		// rts
 
-	}};
-	return dos_type;
-	
+  }};
+  return dos_type;
+  
 }
 
 void get_args(char dos_type) {
-	int ptr1 = 0; // a temporary location for the ASM routine, kickc will make this ZP normally
+  int ptr1 = 0; // a temporary location for the ASM routine, kickc will make this ZP normally
 
-	kickasm(uses ptr1, uses __argc, uses __argv, uses dos_type) {{
+  kickasm(uses ptr1, uses __argc, uses __argv, uses dos_type) {{
 
 .const MAXARGS = 16            // max. amount of arguments in arg. table
 .const CL_SIZE = 64            // command line buffer size
@@ -99,118 +104,130 @@ void get_args(char dos_type) {
 .const XLINE   = $0880
 .const LBUF    = 63            // command line input buffer
 
-.const XDOS         = 4
+.const XDOS    = 4
 .const MAX_DOS_WITH_CMDLINE = XDOS
 
 initmainargs:
-        lda     dos_type      // which DOS?
-        cmp     #MAX_DOS_WITH_CMDLINE + 1
-        bcc     argdos
-        beq     argdos
-        rts
+    lda     dos_type      // which DOS?
+    cmp     #MAX_DOS_WITH_CMDLINE
+    bcc     argdos
+    beq     argdos
+    // no command line parsing with this DOS
+    rts
 
 // Initialize __cl_buffer buffer
 
-argdos: ldy     #ATEOL
-        sty     __cl_buffer+CL_SIZE
+argdos:
+    ldy     #ATEOL
+    sty     __cl_buffer+CL_SIZE
 
 // Move SpartaDOS/XDOS command line to our own buffer
 
-        cmp     #XDOS
-        bne     sparta
+    cmp     #XDOS
+    bne     sparta
 
-        lda     #<XLINE
-        sta     ptr1
-        lda     #>XLINE
-        sta     ptr1+1
-        bne     cpcl0
+    lda     #<XLINE
+    sta     ptr1
+    lda     #>XLINE
+    sta     ptr1+1
+    bne     cpcl0
 
-sparta: lda     DOSVEC
-        clc
-        adc     #<LBUF
-        sta     ptr1
-        lda     DOSVEC+1
-        adc     #>LBUF
-        sta     ptr1+1
+sparta:
+    lda     DOSVEC
+    clc
+    adc     #<LBUF
+    sta     ptr1
+    lda     DOSVEC+1
+    adc     #>LBUF
+    sta     ptr1+1
 
-cpcl0:  ldy     #0
-cpcl:   lda     (ptr1),y
-        sta     __cl_buffer,y
-        iny
-        cmp     #ATEOL
-        beq     movdon
-        cpy     #CL_SIZE
-        bne     cpcl
+cpcl0:
+    ldy     #0
+cpcl:
+    lda     (ptr1),y
+    sta     __cl_buffer,y
+    iny
+    cmp     #ATEOL
+    beq     movdon
+    cpy     #CL_SIZE
+    bne     cpcl
 
-movdon: lda     #0
-        sta     __cl_buffer,y         // null terminate behind ATEOL
+movdon:
+    lda     #0
+    sta     __cl_buffer,y     // null terminate behind ATEOL
 
-// Turn command line into argv table
+    // Turn command line into argv table
 
-        //ldy    #0
-        tay
-eatspc: lda     __cl_buffer,y         // eat spaces
-        cmp     #ATEOL
-        beq     finargs
-        cmp     #SPACE
-        bne     rpar            // begin of argument found
-        iny
-        cpy     #CL_SIZE
-        bne     eatspc
-        beq     finargs         // only spaces is no argument
+    //ldy    #0
+    tay
+eatspc:
+    lda     __cl_buffer,y     // eat spaces
+    cmp     #ATEOL
+    beq     finargs
+    cmp     #SPACE
+    bne     rpar        // begin of argument found
+    iny
+    cpy     #CL_SIZE
+    bne     eatspc
+    beq     finargs     // only spaces is no argument
 
 // Store argument vector
 
-rpar:   lda     __argc          // low-byte
-        asl
-        tax                     // table index
-        tya                     // __cl_buffer index
-        clc
-        adc     #<__cl_buffer
-        sta     __argv,x
-        lda     #>__cl_buffer
-        adc     #0
-        sta     __argv+1,x
-        ldx     __argc
-        inx
-        stx     __argc
-        cpx     #MAXARGS
-        beq     finargs
+rpar:
+    lda     __argc      // low-byte
+    asl
+    tax                 // table index
+    tya                 // __cl_buffer index
+    clc
+    adc     #<__cl_buffer
+    sta     __argv,x
+    lda     #>__cl_buffer
+    adc     #0
+    sta     __argv+1,x
+    ldx     __argc
+    inx
+    stx     __argc
+    cpx     #MAXARGS
+    beq     finargs
 
 // Skip this arg.
 
 skiparg:
-        ldx     __cl_buffer,y
-        cpx     #ATEOL          // end of line?
-        beq     eopar
-        cpx     #SPACE
-        beq     eopar
-        iny
-        cpy     #CL_SIZE
-        bne     skiparg
+    ldx     __cl_buffer,y
+    cpx     #ATEOL      // end of line?
+    beq     eopar
+    cpx     #SPACE
+    beq     eopar
+    iny
+    cpy     #CL_SIZE
+    bne     skiparg
 
 // End of arg. -> place 0
 
 eopar:
-        lda     #0
-        sta     __cl_buffer,y
-        iny                     // y behind arg.
-        cpx     #ATEOL          // was it the last arg?
-        bne     eatspc
+    lda     #0
+    sta     __cl_buffer,y
+    iny                 // y behind arg.
+    cpx     #ATEOL      // was it the last arg?
+    bne     eatspc
 
 // Finish args
 
 finargs:
-        lda     __argc
-        asl
-        tax
-        lda     #0
-        sta     __argv,x
-        sta     __argv+1,x
+    lda     __argc
+    asl
+    tax
+    lda     #0
+    sta     __argv,x
+    sta     __argv+1,x
 
-	}};
+  }};
 }
 
+// These are copied from above ASM defines. Can we have a single definition that covers both?
+#define MAXARGS 16   /* max. amount of arguments in arg. table */
+#define CL_SIZE 64   /* command line buffer size           */
+
 __export __ma int __argc = 0;
-__export char __argv[16 * 2 + 1];
-__export char __cl_buffer[64 + 1];
+__export char __argv[MAXARGS * 2 + 1];
+__export char __cl_buffer[CL_SIZE + 1];
